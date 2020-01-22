8c1c809db3893caefd06e41bfac331e10996fa2d
Sergey Dyachok <sergey@sdyachok.com.ua>
2020-01-22 17:00:32 +0200

    Updated config

---
+++ deploy.php
@@ -0,0 +1,78 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+require 'recipe/rsync.php';
+
+// Project name
+set('application', 'postfix_mail_admin');
+
+// Project repository
+// set('repository', 'git@github.com:dyachoksa/postfix-mail-admin.git');
+set('rsync_src', '.');
+
+// SSH settings
+set('ssh_multiplexing', false);
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+set('shared_files', [".env"]);
+set('shared_dirs', []);
+
+// Writable dirs by web server
+set('writable_dirs', []);
+
+// Configuring the rsync exclusions.
+add('rsync', [
+    /*
+    'exclude' => [
+        '.git',
+        '/.env',
+        '/.venv/',
+        '/node_modules/',
+        '/docs/',
+        '/assets/',
+        'deploy.php',
+    ],
+    */
+    'include' => [
+        '/public/',
+        '/migrations/',
+        '/postfix_mail_admin/',
+        '/poetry.lock',
+        '/pyproject.toml',
+    ],
+]);
+
+
+// Hosts
+
+host('postfix-mail.local')
+    ->hostname('172.23.81.108')
+    ->stage('testing')
+    ->user('sergey')
+    ->set('deploy_path', '/home/app/{{application}}');
+
+
+// Tasks
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'rsync',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
--- postfix_mail_admin/config.py
+++ postfix_mail_admin/config.py
@@ -5,9 +5,13 @@ BASE = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 class Config:
     DEBUG = False
-    SECRET_KEY = "jjY2fy6yjfkvRgwZZ9HV77SES2tEf4xR"
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY", "jjY2fy6yjfkvRgwZZ9HV77SES2tEf4xR"
+    )
 
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{BASE}/app.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", f"sqlite:///{BASE}/app.db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -15,3 +19,9 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_RECORD_QUERIES = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_RECORD_QUERIES = False
