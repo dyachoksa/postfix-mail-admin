211d1722f3dba12a0cfff4cee3ec28c20e3aeca5
Sergey Dyachok <sergey@sdyachok.com.ua>
2020-01-24 19:13:32 +0200

    Added simple dashboard

--- postfix_mail_admin/views/dashboard.py
+++ postfix_mail_admin/views/dashboard.py
@@ -1,10 +1,43 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 
+from ..models import Domain, Mailbox, Alias
+
 dashboard = Blueprint("dashboard", __name__)
 
 
 @dashboard.route("/", endpoint="index")
 @login_required
 def index_page():
-    return render_template("index.html")
+    domains_total = Domain.query.count()
+    domains_active = Domain.query.filter(Domain.is_active == True).count()
+
+    mailboxes_total = Mailbox.query.count()
+    mailboxes_active = Mailbox.query.filter(Mailbox.is_active == True).count()
+
+    aliases_total = Alias.query.count()
+    aliases_active = Alias.query.filter(Alias.is_active == True).count()
+
+    return render_template(
+        "index.html",
+        stats=[
+            {
+                "label": "Domains",
+                "active": domains_active,
+                "total": domains_total,
+                "endpoint": "domains",
+            },
+            {
+                "label": "Mailboxes",
+                "active": mailboxes_active,
+                "total": mailboxes_total,
+                "endpoint": "mailboxes",
+            },
+            {
+                "label": "Aliases",
+                "active": aliases_active,
+                "total": aliases_total,
+                "endpoint": "aliases",
+            },
+        ],
+    )
